6f46e890ee09c68e6f448686c1f7fc91f884fc45
Chris Harvey <chris-harvey94@gmail.com>
2020-07-22 22:27:37 +0100

    changed layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 AOS.init();
 
+
+
 (function () {
 	'use strict';
 
@@ -7,7 +9,7 @@ AOS.init();
 
 	$projects.isotope({
 		itemSelector: '.item',
-		layoutMode: 'fitRows',
+		layoutMode: 'masonry',
 	});
 
 	$('ul.filters > li').on('click', function (e) {
@@ -48,5 +50,26 @@ AOS.init();
 			}
 		});
 	});
+
+	// Init Scrollspy
+    $('body').scrollspy({ target: '#main-nav' });
+
+    // Smooth Scrolling
+    $("#main-nav a").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+
+          window.location.hash = hash;
+        });
+      }
+    });
+	
+
 })(jQuery);
  
