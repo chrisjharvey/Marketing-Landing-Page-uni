9f3ded0866769bf3459f03e759b4c5eb15252f66
cjharvey <chris-harvey94@outlook.com>
2021-05-22 14:57:12 +0100

    changed portfolio layout

--- js/main.js
+++ js/main.js
@@ -1,87 +1,96 @@
-(function () {
-	'use strict';
+;(function () {
+  'use strict'
 
-	// var $projects = $('.projects');
+  var $projects = $('.projects')
 
-	// $projects.isotope({
-	// 	itemSelector: '.item',
-	// 	layoutMode: 'masonry',
-	// });
+  $projects.isotope({
+    itemSelector: '.item',
+    layoutMode: 'masonry',
+  })
 
-	// $('ul.filters > li').on('click', function (e) {
-	// 	e.preventDefault();
+  $('ul.filters > li').on('click', function (e) {
+    e.preventDefault()
 
-	// 	var filter = $(this).attr('data-filter');
+    var filter = $(this).attr('data-filter')
 
-	// 	$('ul.filters > li').removeClass('active');
-	// 	$(this).addClass('active');
+    $('ul.filters > li').removeClass('active')
+    $(this).addClass('active')
 
-	// 	$projects.isotope({ filter: filter });
-	// });
+    $projects.isotope({ filter: filter })
+  })
 
-	$('.card')
-		.mouseenter(function () {
-			$(this).find('.card-overlay').css({ top: '-100%' });
-			$(this).find('.card-hover').css({ top: '0' });
-		})
-		.mouseleave(function () {
-			$(this).find('.card-overlay').css({ top: '0' });
-			$(this).find('.card-hover').css({ top: '100%' });
-		});
+  $('.card')
+    .mouseenter(function () {
+      $(this).find('.card-overlay').css({ top: '-100%' })
+      $(this).find('.card-hover').css({ top: '0' })
+    })
+    .mouseleave(function () {
+      $(this).find('.card-overlay').css({ top: '0' })
+      $(this).find('.card-hover').css({ top: '100%' })
+    })
 
-	function checkScroll() {
-		if ($(this).scrollTop() > 200) {
-			$('.navbar').addClass('solid');
-		} else {
-			$('.navbar').removeClass('solid');
-		}
-	}
+  function checkScroll() {
+    if ($(this).scrollTop() > 200) {
+      $('.navbar').addClass('solid')
+    } else {
+      $('.navbar').removeClass('solid')
+    }
+  }
 
-	$(document).ready(function () {
-		checkScroll();
-		$(window).scroll(checkScroll);
-		$('.navbar-toggler').click(function () {
-			if ($(window).scrollTop() <= 300) {
-				$('nav.navbar').toggleClass('solid-toggle');
-			}
-		});
-	});
+  $(document).ready(function () {
+    checkScroll()
+    $(window).scroll(checkScroll)
+    $('.navbar-toggler').click(function () {
+      if ($(window).scrollTop() <= 300) {
+        $('nav.navbar').toggleClass('solid-toggle')
+      }
+    })
+  })
 
-	//Scroll Spy
-	$(document).ready(function () {
-		var scrollLink = $('.scroll');
+  //Scroll Spy
+  $(document).ready(function () {
+    var scrollLink = $('.scroll')
 
-		// Smooth scrolling
-		scrollLink.click(function (e) {
-			e.preventDefault();
-			$('body,html').animate(
-				{
-					scrollTop: $(this.hash).offset().top,
-				},
-				1000
-			);
-		});
+    // Smooth scrolling
+    scrollLink.click(function (e) {
+      e.preventDefault()
+      $('body,html').animate(
+        {
+          scrollTop: $(this.hash).offset().top,
+        },
+        1000
+      )
+    })
 
-		// Active link switching
-		$(window).scroll(function () {
-			var scrollbarLocation = $(this).scrollTop();
+    // Active link switching
+    $(window).scroll(function () {
+      var scrollbarLocation = $(this).scrollTop()
 
-			scrollLink.each(function () {
-				var sectionOffset = $(this.hash).offset().top - 20;
+      scrollLink.each(function () {
+        var sectionOffset = $(this.hash).offset().top - 20
 
-				if (sectionOffset <= scrollbarLocation) {
-					$(this).parent().addClass('active');
-					$(this).parent().siblings().removeClass('active');
-				}
-			});
-		});
-	});
+        if (sectionOffset <= scrollbarLocation) {
+          $(this).parent().addClass('active')
+          $(this).parent().siblings().removeClass('active')
+        }
+      })
+    })
+  })
 
-	// AOS
-	AOS.init({
-		duration: 1000,
-		easing: 'ease-in',
-		once: true,
-		mirror: false,
-	});
-})(jQuery);
+  $(function () {
+    var navMain = $('.navbar-collapse') // avoid dependency on #id
+    // "a:not([data-toggle])" - to avoid issues caused
+    // when you have dropdown inside navbar
+    navMain.on('click', 'a:not([data-toggle])', null, function () {
+      navMain.collapse('hide')
+    })
+  })
+
+  // AOS
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in',
+    once: true,
+    mirror: false,
+  })
+})(jQuery)
